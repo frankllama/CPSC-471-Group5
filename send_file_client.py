a3a4e4dee681a26a8c4ad1c492da0d28ef1566ca
frankllama <chewfasa@gmail.com>
2024-04-21 17:45:54 -0700

    Added client and server python files. These are just base files with comments to outline code structure.

---
+++ send_file_client.py
@@ -0,0 +1,21 @@
+# import libraries
+
+# command line check for number of arguments
+
+# assign server address
+
+# assign server port
+
+# open the file in byte mode
+
+# create a TCP socket
+
+# connect to the server
+
+# read in bytes of data from the file object
+
+# print the data to confirm
+
+# send the data
+
+# close the socket and the file
---
+++ send_file_server.py
@@ -0,0 +1,17 @@
+# import libraries
+
+# assign the port on which to listen
+
+# create a welcome socket
+
+# bind the socket to the port
+
+# start listening on the socket
+
+# accept connections
+
+# receive file byte data and decode
+
+# print data to confirm
+
+# close the socket
