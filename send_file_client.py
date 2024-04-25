625217bdebe9703812b89778119f45252ad0ab84
frankllama <chewfasa@gmail.com>
2024-04-24 18:56:10 -0700

    Started from barebones tcp files and got ls command to work.

--- send_file_client.py
+++ send_file_client.py
@@ -1,90 +1,30 @@
-#!/usr/bin/env python
+# Reference Chapter 2.7.2 pg. 159-164
 
-import socket
-import sys
 import os
+import sys
+import socket
 
-def create_data_socket():
-    """Create a data socket for transfer and return its port."""
-    data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    data_socket.bind(('', 0))  # Bind to any available port
-    data_socket.listen(1)
-    return data_socket, data_socket.getsockname()[1]
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
 
-def handle_get(control_socket, file_name):
-    """Handle the 'get' command to download a file from the server."""
-    data_socket, data_port = create_data_socket()
-    control_socket.sendall(f"get {file_name} {data_port}".encode())
-    conn, _ = data_socket.accept()
-    with open(file_name, 'wb') as file:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            file.write(data)
-    conn.close()
-    data_socket.close()
-    print(f"Downloaded '{file_name}' successfully.")
+# The name of the file
+# fileName = sys.argv[1]
+# Open the file in byte mode
+# fileObj = open(fileName, "rb")
 
-def handle_put(control_socket, file_name):
-    """Handle the 'put' command to upload a file to the server."""
-    if not os.path.exists(file_name):
-        print("File not found.")
-        return
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
 
-    data_socket, data_port = create_data_socket()
-    control_socket.sendall(f"put {file_name} {data_port}".encode())
-    conn, _ = data_socket.accept()
-    with open(file_name, 'rb') as file:
-        while (data := file.read(1024)):
-            conn.sendall(data)
-    conn.close()
-    data_socket.close()
-    print(f"Uploaded '{file_name}' successfully.")
-    # Send acknowledgment to server
-    control_socket.sendall("ACK".encode())
+# fileData = fileObj.read(65536)
 
+# clientSocket.send(fileData)
 
-def handle_ls(control_socket):
-    """Handle the 'ls' command to list files on the server."""
-    data_socket, data_port = create_data_socket()
-    control_socket.sendall(f"ls {data_port}".encode())
-    conn, _ = data_socket.accept()
-    data = conn.recv(1024).decode()
+command = input("ftp> ")
+if command == "ls":
+    clientSocket.send(command.encode())
+    data = clientSocket.recv(1024).decode()
     print(data)
-    control_socket.sendall("ACK".encode())
-    conn.close()
-    data_socket.close()
+    # connection.close()
 
-def main(server_ip, server_port):
-    control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    control_socket.connect((server_ip, server_port))
-    print("Connected to FTP server.")
-
-    try:
-        while True:
-            command = input("ftp> ")
-            if command.startswith("get "):
-                file_name = command.split()[1]
-                handle_get(control_socket, file_name)
-            elif command.startswith("put "):
-                file_name = command.split()[1]
-                handle_put(control_socket, file_name)
-            elif command.startswith("ls"):
-                handle_ls(control_socket)
-            elif command.strip() == "quit":
-                control_socket.sendall("quit".encode())
-                break
-            else:
-                print("Unknown command.")
-    finally:
-        control_socket.close()
-        print("Connection closed.")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: cli.py <server machine> <server port>")
-        sys.exit(1)
-    server = sys.argv[1]
-    port = int(sys.argv[2])
-    main(server, port)
+clientSocket.close()
+# fileObj.close()
--- send_file_server.py
+++ send_file_server.py
@@ -1,86 +1,24 @@
-#!/usr/bin/env python
+# Reference Chapter 2.7.2 pg. 159-164
 
-import socket
-import threading
 import os
+import sys
+import socket
 
-def handle_client(client_socket):
-    try:
-        while True:
-            command = client_socket.recv(1024).decode()
-            if not command:
-                break
-            print(f"Received command: {command}")
-            if command.startswith('put'):
-                _, file_name, data_port = command.split()
-                handle_put(client_socket, file_name, int(data_port))
-            elif command.startswith('get'):
-                _, file_name, data_port = command.split()
-                handle_get(client_socket, file_name, int(data_port))
-            elif command.startswith('ls'):
-                _, data_port = command.split()
-                handle_ls(client_socket, int(data_port))
-            elif command == 'quit':
-                client_socket.sendall("SUCCESS: Goodbye".encode())
-                break
-    finally:
-        client_socket.close()
+serverPort = int(sys.argv[1])
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+serverSocket.bind(('0.0.0.0', serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
 
-def handle_put(control_socket, file_name, data_port):
-    data_socket = setup_data_connection(data_port)
-    with open(file_name, 'wb') as file:
-        while True:
-            data = data_socket.recv(1024)
-            if not data:
-                break
-            file.write(data)
-    data_socket.close()
-    control_socket.sendall(f"SUCCESS: {file_name} uploaded".encode())
-
-def handle_get(control_socket, file_name, data_port):
-    if os.path.exists(file_name):
-        data_socket = setup_data_connection(data_port)
-        with open(file_name, 'rb') as file:
-            while True:
-                data = file.read(1024)
-                if not data:
-                    break
-                data_socket.sendall(data)
-        data_socket.close()
-        control_socket.sendall(f"SUCCESS: {file_name} sent".encode())
-    else:
-        control_socket.sendall("FAILURE: File not found".encode())
-
-def handle_ls(control_socket, data_port):
-    data_socket = setup_data_connection(data_port)
-    files = os.listdir()
-    formatted_file_list = '\n'.join(files)
-    data_socket.sendall(formatted_file_list.encode())
-    data_socket.close()
-    control_socket.sendall("SUCCESS: List sent".encode())
-
-def setup_data_connection(data_port):
-    data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    data_socket.bind(('0.0.0.0', data_port))
-    data_socket.listen(1)
-    connection, _ = data_socket.accept()
-    return connection
-
-def start_server(port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', port))
-    server_socket.listen(5)
-    print(f"")
-    print(f"FTP server listening on port {port}")
-
-    try:
-        while True:
-            client_socket, addr = server_socket.accept()
-            print(f"Control connection established with {addr}")
-            threading.Thread(target=handle_client, args=(client_socket,)).start()
-    finally:
-        server_socket.close()
-
-if __name__ == "__main__":
-    import sys
-    start_server(int(sys.argv[1]))
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    command = connectionSocket.recv(65536).decode()
+    if command == "ls":
+        # os.system(command)
+        files = os.listdir()
+        formated_file_list = '\n'.join(files)
+        connectionSocket.send(formated_file_list.encode())
+    # f = open("wedidit", "w")
+    # f.write(fileData)
+    connectionSocket.close()
+    break
