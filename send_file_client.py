7045775109ab37ae0e021f8bd1422d17a7d25a56
frankllama <chewfasa@gmail.com>
2024-05-03 15:06:26 -0700

    Code clean up. Got all commands to work together for both txt and jpg files. TODO: error handling.

--- send_file_client.py
+++ send_file_client.py
@@ -1,48 +1,33 @@
-# Reference Chapter 2.7.2 pg. 159-164
-
 import os
 import sys
 import socket
 from pathlib import Path
+import time
 
+# Get server address and port from command line arguments
 serverName = sys.argv[1]
 serverPort = int(sys.argv[2])
-
-# The name of the file
-# fileName = sys.argv[1]
-# Open the file in byte mode
-# fileObj = open(fileName, "rb")
-
+# Create a TCP socket and connect to the server
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-
-# fileData = fileObj.read(65536)
-
-# clientSocket.send(fileData)
-
-# Specify client directory for client side files.
-# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-# client_files = "clientFilesReceived".format(BASE_DIR)
-# Other way to Specify directory
-# ROOT_DIR = Path(__file__).parent
-# client_files = ROOT_DIR / 'clientFilesReceived'
 
 while True:
     command = input("ftp> ")
     command = command.split()
     print("command[0]: ", command[0])
     if command[0] == "ls":
-        command = " ".join(str(element) for element in command)
+        # command = " ".join(str(element) for element in command)
+        command = str(1*" ").join(command)
         clientSocket.send(command.encode())
         data = clientSocket.recv(1024).decode()
         print(data)
-        # connection.close()
     elif command[0] == "get":
-        command = " ".join(str(element) for element in command)
+        # command = " ".join(str(element) for element in command)
+        command = str(1*" ").join(command)
         # Send command and file name to server
         clientSocket.send(command.encode())
         # Receive filename confirmation and filesize from server.
-        data = clientSocket.recv(1024).decode("utf-8")
+        data = clientSocket.recv(1024).decode()
         print("filename and filesize received from server: ", data)
         item = data.split("_")
         fileName = item[0]
@@ -55,56 +40,29 @@ while True:
                 if not data:
                     break
                 dataBuffer += data
-                # print("file data received: ", data.decode())
-                # f.write(data.decode())
-                # break
             dataBuffer = bytes(dataBuffer)
             f.write(dataBuffer)
-            # dataBuffer = str(dataBuffer, 'utf-8')
-            # dataBuffer = dataBuffer.lstrip("b\'")
-            # dataBuffer = dataBuffer.rstrip("\n'")
-            # f.write(dataBuffer)
             f.close()
-        # data_buffer = bytes()
-        # while True:
-        #     data = clientSocket.recv(1024)
-        #     data_buffer += data
-        #     if not data:
-        #         break
-        # # data = bytes(data_buffer)
-        # data = data.decode()
-        # with open("temp",'w') as f:
-        #     f.write(data)
-        # f.close()
+        print("Successfully received data from server.")
     elif command[0] == "put":
         # command = " ".join(str(element) for element in command)
-        command = " ".join(command)
-        clientSocket.send(command.encode())
-        # command = ' '.join(command)
-        # print("put command string joined: ", command)
-        # command = clientSocket.recv(1024).decode()
-        # print("File Path: ", client_files)
-        # print("command length: ", len(command.split()))
-        # if len(command.split()) == 2:
-            # fileName = os.path.join(client_files, command[1])
-            # fileName = Path(client_files, command[1])
+        # command = " ".join(command)
+        command = str(1*" ").join(command)
+        clientSocket.sendall(command.encode())
         command = command.split()
+        # Specify client directory for client side files.
         p = Path(__file__).parent.resolve()
-        p = p / 'clientFilesReceived' / command[1]
+        p = p / 'clientFiles' / command[1]
         # fileName = os.path.isfile(p)
         fileName = p
         print("fileName: ", fileName)
         fileSize = os.path.getsize(fileName)
-        data = f"{fileName}_{fileSize}"
-        # data = f"{command[1]}_{fileSize}"
+        print("fileSzie from os.path.getsize: ", fileSize)
+        data = f"{Path(fileName).name}_{fileSize}"
         print("data to send to server: ", data)
         clientSocket.send(data.encode("utf-8"))
-        # clientSocket.send(data.encode())
-        # if os.path.isfile(fileName):
-            # fileSize = os.path.getsize(fileName)
+        time.sleep(1)
         print("fileSize: ", fileSize)
-            # p = Path(__file__).parent.resolve()
-            # p = p / 'clientFilesReceived' / command[1]
         print("file path: ", p)
         byte_fileData = bytearray(fileSize)
         numSent = 0
@@ -112,10 +70,15 @@ while True:
             data = f.read(fileSize)
             while len(byte_fileData) > numSent:
                 numSent += clientSocket.send(data[numSent:])
+            f.close()
+        print("Finished sending data to server.")
         # else:
         #     print("Please just pass the name of one file at a time.")
     elif command[0] == "quit":
         command = " ".join(str(element) for element in command)
         clientSocket.send(command.encode())
         clientSocket.close()
+        print("Successfully closed client socket and sent server request to close connection.")
         break
+    else:
+        pass
--- send_file_server.py
+++ send_file_server.py
@@ -1,86 +1,68 @@
-# Reference Chapter 2.7.2 pg. 159-164
-
 import os
 from os.path import isfile
 import sys
 import socket
 from pathlib import Path
 
+# Get the port to listen on
 serverPort = int(sys.argv[1])
+# Create a welcome socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+# Bind the socket to the port
 serverSocket.bind(('0.0.0.0', serverPort))
+# Start listening to the socket
 serverSocket.listen(1)
 print('The server is ready to receive')
+# Accept the connection
 connectionSocket, addr = serverSocket.accept()
 # Specify server directory for server files.
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 server_files = "serverFiles".format(BASE_DIR)
 
 while True:
-    # connectionSocket, addr = serverSocket.accept()
     command = connectionSocket.recv(1024).decode()
     command = command.split()
-    print("command received: ", command)
+    print("Command received from Client: ", command)
     if command[0] == "ls":
-        # os.system(command)
         files = os.listdir()
         formated_file_list = '\n'.join(files)
         connectionSocket.send(formated_file_list.encode())
     elif command[0] == "get":
-        print("File Path: ", server_files)
+        print("File Path to get file: ", server_files)
         print("command length: ", len(command))
         if len(command) == 2:
-            # if not os.path.exists(server_files):
-            #     os.makedirs(server_files)
-            
             # Todo: check if file exists and send it.
             fileName = os.path.join(server_files, command[1])
-            print("fileName: ", fileName)
+            print("FileName found: ", fileName)
             fileSize = os.path.getsize(fileName)
             data = f"{fileName}_{fileSize}"
             connectionSocket.send(data.encode("utf-8"))
             if os.path.isfile(fileName):
-                # relpath = os.path.relpath(fileName, server_files)
                 fileSize = os.path.getsize(fileName)
                 print("fileSize: ", fileSize)
                 p = Path(__file__).parent.resolve()
                 p = p / 'serverFiles' / command[1]
-                # p = Path(__file__).with_name('serverFiles' / command[1])
                 print("file path: ", p)
-                # other way
                 byte_fileData = bytearray(fileSize)
                 numSent = 0
                 with p.open('rb') as f:
-                    # while True:
-                    #     data = f.read(fileSize)
-                    #     if not data:
-                    #         break
-                    #     # print("data before sending:", data)
-                    #     connectionSocket.send(data)
-                    #     break
-                    # f.close()
-                    # other way
                     data = f.read(fileSize)
                     while len(byte_fileData) > numSent:
                         numSent += connectionSocket.send(data[numSent:])
+                    f.close()
+                print("Finished sending file to client.")
         else:
             print("Please just pass the name of 1 file at a time.")
-            # f = open("wedidit", "w")
-            # f.write(fileData)
     elif command[0] == "put":
-        # command = " ".join(str(element) for element in command)
-        # Send command and file name to client
-        # connectionSocket.send(command.encode())
-        # Receive filename confirmation and filesize from client.
-        # data = connectionSocket.recv(1024).decode("utf-8")
-        data = connectionSocket.recv(1024).decode()
+        data = connectionSocket.recv(1024)
+        print("data in put received: ", data)
+        data = data.decode()
         print("filename and filesize received from client: ", data)
         item = data.split("_")
         fileName = item[0]
         fileSize = int(item[1])
         print("filesize to receive: ", fileSize)
         p = Path(__file__).parent.resolve()
-        # p = p / 'serverFiles' / 'fileFromClient'
         print("file path: ", p)
         dataBuffer = bytearray()
         with open("fileFromClient", "wb") as f:
@@ -92,6 +74,10 @@ while True:
             dataBuffer = bytes(dataBuffer)
             f.write(dataBuffer)
             f.close()
+        print("Successfully retrieved data sent from client.")
     elif command[0] == "quit":
         connectionSocket.close()
+        print("Successfully closed socket and ended connection from client request.")
         break
+    else:
+        pass
